7d9d11c089bd94115f647e2d6da8d899299d8404
vtzvetkov7 <vtzvetkov7@gmail.com>
2026-03-22 10:11:30 -0700

    Initial State
    Added API versioning support and updated comments.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
+using Asp.Versioning;
 using ProductCatalogApi.Data;
 using ProductCatalogApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// This section is for WebApplicationBuilder class (builder object)
+// Put here statements for Swagger generation, repository registration or API versioning 
+
+
 var app = builder.Build();
 
+// Mapping section
 app.MapGet("/", () => "Product Catalog API is running");
+
+
+// Versioning section
+
 
 app.Run();
